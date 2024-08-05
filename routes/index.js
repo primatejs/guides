390bb69adbcbd748b969863b72ac006b04649aff
terrablue <102580937+terrablue@users.noreply.github.com>
2024-08-05 17:39:54 +0200

    implementation

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
-import ffmpeg from "fluent-ffmpeg";
+import file from "@rcompat/fs/file";
+import { txt } from "@rcompat/http/mime";
+import spawn from "@rcompat/stdio/spawn";
+
+const command = "ffmpeg";
+const input = "-i pipe:0";
+const flags = "-acodec aac -vcodec libx264 -movflags frag_keyframe+empty_moov";
+const output = "-f mp4 pipe:1";
+const filename = "/tmp/test.avi";
 
 export default {
-  get() {
-    ffmpeg("/tmp/test.avi")
-      .on('progress', function(progress) {
-        console.log('Processing: ' + progress.percent + '% done');
-      });
-    return "hi";
+  get(request) {
+    const { stdout, stdin } = spawn(`${command} ${input} ${flags} ${output}`);
+
+    file(filename).stream().pipeTo(stdin);
+
+    return new Response(stdout, { headers: { "context-type": txt }});
   }
 }
