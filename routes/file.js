a1076291b7d28f9debb4a01b1a585b0bea5dce0a
terrablue <102580937+terrablue@users.noreply.github.com>
2024-08-06 00:13:59 +0200

    readd support for loading from file

--- routes/file.js
+++ routes/file.js
@@ -1,3 +1,4 @@
+import file from "@rcompat/fs/file";
 import { mp4 } from "@rcompat/http/mime";
 import spawn from "@rcompat/stdio/spawn";
 
@@ -6,13 +7,13 @@ const input = "-i pipe:0";
 const flags = "-acodec aac -vcodec libx264 -movflags frag_keyframe+empty_moov";
 const output = "-f mp4 pipe:1";
 const repo = "primatejs/guides";
-const url = `https://raw.githubusercontent.com/${repo}/ffmpeg/assets/test.avi`;
+const filename = `${import.meta.url}/../../server/assets/test.avi`;
 
 export default {
   async get(request) {
     const { stdout, stdin } = spawn(`${command} ${input} ${flags} ${output}`);
 
-    (await fetch(url)).body.pipeTo(stdin);
+    file(filename).stream().pipeTo(stdin);
 
     return new Response(stdout, { headers: { "content-type": mp4 }});
   }
